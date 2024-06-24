3f9ddc969908d4cdf20b333c3469827adc22cd3b
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-06-24 10:16:47 +0530

    small changes

--- frontend/src/components/services.js
+++ frontend/src/components/services.js
@@ -3,8 +3,14 @@ import axios from 'axios';
 
 const api_URL = 'http://localhost:3333';
 
-export const test = () => {
-    axios.get(`${api_URL}/api/test`)
+export const test = async () => {
+    return await axios.get(`${api_URL}/api/test`)
         .then(response => console.log(response))
-        .catch(error => alert('server is down'))
-}
+        .catch(error => {
+            console.log("Error msg\n", error);
+            alert('server is down');
+        })
+
+};
+
+// for jwt tokens
